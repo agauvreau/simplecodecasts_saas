db202a1694aa0e499751b9aebc353f5491e16257
Adam Gauvreau <adamgauvreau@hotmail.com>
2016-01-08 18:49:37 +0000

    Improved user index page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,11 +6,11 @@ module UsersHelper
            "<i class='fa fa-code'></i>".html_safe
        elsif
             @user.profile.job_title == "Entrepreneur"
-           "<i class='fa lightbulb-o'></i>".html_safe
+           "<i class='fa fa-lightbulb-o'></i>".html_safe
         
         elsif 
             @user.profile.job_title == "Investor"
-           "<i class='fa dollar'></i>".html_safe
+           "<i class='fa fa-dollar'></i>".html_safe
        end
        
     end
